ad2e96dfd7a84c0577193104dcc2ba570601aea0
João Victor Kikuti <joaovictorkikuti@gmail.com>
2021-05-12 20:48:42 -0300

    código arrumado requisito 4

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -1,6 +1,7 @@
 /* eslint-disable max-len */
 /* eslint-disable no-unused-vars */
 
+const { strictEqual } = require('assert');
 const assert = require('assert');
 const circle = require('../src/circle');
 
@@ -27,18 +28,18 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
   it('Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados', () => {
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
-    assert.deepStrictEqual(circle(1), {"area": 3.14, "circumference": 6.28, "radius": 1});
+    assert.strictEqual(typeof circle(1), 'object');
     // Teste se o objeto retornado tem 3 entradas.
-    assert.deepStrictEqual(Object.keys(circle(1)).length, 3);
+    assert.strictEqual(Object.keys(circle(1)).length, 3);
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    assert.deepStrictEqual(circle(), undefined);
+    assert.strictEqual(circle(), undefined);
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
     let radius = 2;
-    assert.deepStrictEqual(circle(radius).circumference, 2 * 3.14 * radius);
+    assert.strictEqual(circle(radius).circumference, 2 * 3.14 * radius);
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
     radius = 3;
-    assert.deepStrictEqual(circle(radius).area, 3.14 * radius * radius);
+    assert.strictEqual(circle(radius).area, 3.14 * radius * radius);
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
-    assert.deepStrictEqual(circle(radius), {radius: 3, area: 3.14 * radius * radius, circumference: 2 * 3.14 * radius});
+    assert.deepStrictEqual(circle(radius), { radius: 3, area: 3.14 * radius * radius, circumference: 2 * 3.14 * radius });
   });
 });
